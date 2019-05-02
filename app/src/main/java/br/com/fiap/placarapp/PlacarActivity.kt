fd0d18bcbf1933c5452b73b81f9ecafdcc9e170a
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-02 20:54:47 -0300

    aula 2

--- app/src/main/java/br/com/fiap/placarapp/PlacarActivity.kt
+++ app/src/main/java/br/com/fiap/placarapp/PlacarActivity.kt
@@ -1,5 +1,7 @@
 package br.com.fiap.placarapp
 
+import android.app.Activity
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_placar.*
@@ -9,24 +11,28 @@ class PlacarActivity : AppCompatActivity() {
     private var golCasa = 0
     private var golVisitante = 0
 
+    private lateinit var placarViewModel: PlacarViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placar)
 
+        placarViewModel = ViewModelProviders.of(this).get(PlacarViewModel::class.java)
+
         tvTimeCasa.text = intent.getStringExtra("TIME_CASA")
         tvTimeVisitante.text = intent.getStringExtra("TIME_VISITANTE")
 
-        tvPlacarCasa.text = "$golCasa"
-        tvPlacarVisitante.text = "$golVisitante"
+        tvPlacarCasa.text = "${placarViewModel.golCasa}"
+        tvPlacarVisitante.text = "${placarViewModel.golVisitante}"
 
         btGolCasa.setOnClickListener{
-            golCasa ++
-            tvPlacarCasa.text = "$golCasa"
+            placarViewModel.golCasa ++
+            tvPlacarCasa.text = "${placarViewModel.golCasa}"
         }
 
         btGolVisitante.setOnClickListener {
-            golVisitante ++
-            tvPlacarVisitante.text = "$golVisitante"
+            placarViewModel.golVisitante ++
+            tvPlacarVisitante.text = "${placarViewModel.golVisitante}"
         }
     }
 }
---
+++ app/src/main/java/br/com/fiap/placarapp/PlacarViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.fiap.placarapp
+
+import android.arch.lifecycle.ViewModel
+
+class PlacarViewModel: ViewModel(){
+
+    var golCasa = 0
+    var golVisitante = 0
+}
